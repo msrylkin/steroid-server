5ed9d22dfd5b920471a3b0ac3f5261960d7535c3
Max Max <m.rylkin@osome.com>
2023-08-11 23:49:26 +0300

    deploy

--- src/sdk/s3.sdk.ts
+++ src/sdk/s3.sdk.ts
@@ -1,12 +1,11 @@
-import * as AWS from "aws-sdk";
+import { S3 } from "@aws-sdk/client-s3";
 
-const isAws = !!process.env.STAGE;
+// const isAws = !!process.env.STAGE;
 
-const S3 = new AWS.S3({
-    s3ForcePathStyle: true,
-    accessKeyId: isAws ? undefined : 'S3RVER', // This specific key is required when working offline
-    secretAccessKey: isAws ? undefined : 'S3RVER',
-    endpoint: isAws ? undefined : new AWS.Endpoint('http://localhost:4569'),
-});
+// const S3 = new S3Class({
+//     accessKeyId: isAws ? undefined : 'S3RVER', // This specific key is required when working offline
+//     secretAccessKey: isAws ? undefined : 'S3RVER',
+//     endpoint: isAws ? undefined : new AWS.Endpoint('http://localhost:4569'),
+// });
 
 export { S3 };
