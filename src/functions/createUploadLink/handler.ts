a297e84b0a69c5438c744602efec716bdae9c118
Max Max <m.rylkin@osome.com>
2023-08-12 00:01:05 +0300

    deploy

--- src/functions/createUploadLink/handler.ts
+++ src/functions/createUploadLink/handler.ts
@@ -1,15 +1,14 @@
 import { ValidatedEventAPIGatewayProxyEvent } from "@libs/apiGateway";
 import schema from "./schema";
-import { S3 } from 'src/sdk/s3.sdk';
+import { S3, getPutPresignedUrl } from 'src/sdk/s3.sdk';
 import { middyfy } from '@libs/lambda';
 
 const createUploadLink: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
     console.log('commit', event.body.commit);
     const randomID = parseInt(Math.random() * 10000000 + '');
-    const signedUrl = await S3.getSignedUrlPromise('putObject', {
+    const signedUrl = await getPutPresignedUrl({
         Bucket: 'sources-archives',
         Key: `sources/commit:${event.body.commit}/${randomID}`,
-        Expires: 60 * 5 * 100000000,
         ContentType: 'application/zip',
         Metadata: {
             commit: event.body.commit,
--- src/sdk/s3.sdk.ts
+++ src/sdk/s3.sdk.ts
@@ -1,11 +1,17 @@
-import { S3 } from "@aws-sdk/client-s3";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
-// const isAws = !!process.env.STAGE;
+const isAws = !!process.env.STAGE;
 
-// const S3 = new S3Class({
-//     accessKeyId: isAws ? undefined : 'S3RVER', // This specific key is required when working offline
-//     secretAccessKey: isAws ? undefined : 'S3RVER',
-//     endpoint: isAws ? undefined : new AWS.Endpoint('http://localhost:4569'),
-// });
+export const S3 = new S3Client({
+    credentials: {
+        accessKeyId: isAws ? undefined : 'S3RVER', // This specific key is required when working offline
+        secretAccessKey: isAws ? undefined : 'S3RVER',
+    },
+    endpoint: isAws ? undefined : 'http://localhost:4569',
+});
 
-export { S3 };
+export function getPutPresignedUrl(input: ConstructorParameters<typeof PutObjectCommand>[0]) {
+    const command = new PutObjectCommand(input);
+    return getSignedUrl(S3, command, { expiresIn: 15 * 60 });
+}
