aa8c5fc9784a4f2557c917f6c7067407e768086c
Max Max <m.rylkin@osome.com>
2023-08-12 00:13:22 +0300

    deploy

--- src/sdk/s3.sdk.ts
+++ src/sdk/s3.sdk.ts
@@ -4,11 +4,11 @@ import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 const isAws = !!process.env.STAGE;
 
 export const S3 = new S3Client({
-    credentials: {
-        accessKeyId: isAws ? undefined : 'S3RVER', // This specific key is required when working offline
-        secretAccessKey: isAws ? undefined : 'S3RVER',
-    },
-    endpoint: isAws ? undefined : 'http://localhost:4569',
+    // credentials: {
+    //     accessKeyId: isAws ? undefined : 'S3RVER', // This specific key is required when working offline
+    //     secretAccessKey: isAws ? undefined : 'S3RVER',
+    // },
+    // endpoint: isAws ? undefined : 'http://localhost:4569',
 });
 
 export function getPutPresignedUrl(input: ConstructorParameters<typeof PutObjectCommand>[0]) {
